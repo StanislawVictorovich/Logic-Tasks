86b1a542694ac32bd086aeb0f9c25d6389527455
Shohrukh Davlatov <shohrukh.92@gmail.com>
2018-10-24 17:41:12 +0300

    Update minmasarray.js

--- minmasarray.js
+++ minmasarray.js
@@ -1,6 +1,7 @@
 const arr = [4, 67, 7, 90, 1, 45, 78, 92, 64];
 
 const minMasArray = (array) => {
+    // можно решить одним циклом, только небольшой баг в инициализации этих переменных
     let min=0, max=0;
     for(i = 0; i < array.length; i++){
         if (array[i] < min) min = array[i];
