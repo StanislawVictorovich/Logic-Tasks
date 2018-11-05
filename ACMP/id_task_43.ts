6cd3c7af388a83300a11ec870bf4d604877c8862
Stanislav Svirepov <svirepov.stanislav@gmail.com>
2018-11-05 15:17:51 +0200

    id task 43

--- ACMP/id_task_43.ts
+++ ACMP/id_task_43.ts
@@ -1,18 +1,28 @@
-const arr = [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+let list: number[] = new Array (0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0);
 
 /**
  * @description calculating the biggest sequence of nulls in array
  * @param {Array} array 
  */
-const numberOfNulls = (array) => {
-    let nullCounter = 0, result = 0;
-    for (let i = 0; i < array.length; i++) {
-        !array[i] ? nullCounter++ : nullCounter = 0;
-        if (nullCounter > result) {
-            result = nullCounter;
-        }
+class numberOfNulls {
+    private nullCounter: number;
+    private result: number;
+    private arr: number[] = new Array();
+    constructor (a: number[]) {
+      this.nullCounter = 0;
+      this.result = 0;
+      this.arr = a;
     }
-    return result;
+    public get calculate(): number {
+      for (let i: number = 0; i < this.arr.length; i++) {
+        !this.arr[i] ? this.nullCounter++ : this.nullCounter = 0;
+        if (this.nullCounter > this.result) {
+          this.result = this.nullCounter;
+        }
+      }
+      return this.result;
+    }
 }
 
-console.log(numberOfNulls(arr));
+const o = new numberOfNulls(list);
+console.log(o.calculate);
