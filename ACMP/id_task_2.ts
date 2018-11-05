1beeb6282c7e4cf90771c37f2daea7305de64659
Stanislav Svirepov <svirepov.stanislav@gmail.com>
2018-11-05 14:42:26 +0200

    id task 2

---
+++ ACMP/id_task_2.ts
@@ -0,0 +1,24 @@
+const number: number = 5;
+
+/**
+ * @description adding all elements in sequense 0 ... N
+ * @constructor inputs a number
+ * @returns method calculate returns result of adding elements
+ */
+class Summator {
+    private readonly n: number = 0;
+    constructor (n: number) {
+        this.n = n;
+    }
+    get calculate(): number {
+        let i: number = this.n, result: number = 0;
+        while (i) {
+            result += i--;
+        }
+        return result;
+    }
+}
+
+let a = new Summator(number);
+
+console.log(a.calculate);
