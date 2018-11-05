7e175c55607ff4c93807665578dc173f6dbdc80f
Stanislav Svirepov <svirepov.stanislav@gmail.com>
2018-11-05 10:04:47 +0200

    id task 336

--- ACMP/id_task_336.js
+++ ACMP/id_task_336.js
@@ -1,5 +1,10 @@
 const arr = [1, 2, 2, 2, 1, 1, 2, 1, 2, 1, 2];
 
+/**
+ * @description calculating amount of floors in house 
+ * @param {Array} array sequense of pushing buttons in lift
+ * @returns amount of floors in house 
+ */
 const lift = (array) => {
     let numberOfFloors = 0, floorsCounter = 0;
     for (let i = 0; i < array.length; i++) {
