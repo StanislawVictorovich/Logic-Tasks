d77d0831877e81707cb4cec38ad80f7257a9f1c9
Stanislav Svirepov <svirepov.stanislav@gmail.com>
2018-11-05 08:42:16 +0200

    id task 43

---
+++ ACMP/id_task_43.js
@@ -0,0 +1,14 @@
+const arr = [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+
+const numberOfNulls = (array) => {
+    let nullCounter = 0, result = 0;
+    for (let i = 0; i < array.length; i++) {
+        !array[i] ? nullCounter++ : nullCounter = 0;
+        if (nullCounter > result) {
+            result = nullCounter;
+        }
+    }
+    return result;
+}
+
+console.log(numberOfNulls(arr));
