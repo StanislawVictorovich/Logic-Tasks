7e9b69814b2cee59a4648ed44f544139bba7d115
Stanislav Svirepov <svirepov.stanislav@gmail.com>
2018-11-05 09:58:09 +0200

    id task 43

--- ACMP/id_task_43.js
+++ ACMP/id_task_43.js
@@ -1,5 +1,9 @@
 const arr = [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
 
+/**
+ * @description calculating the biggest sequence of nulls in array
+ * @param {Array} array 
+ */
 const numberOfNulls = (array) => {
     let nullCounter = 0, result = 0;
     for (let i = 0; i < array.length; i++) {
