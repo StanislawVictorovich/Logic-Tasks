5e79d82e2a5c638c5637b82b547f474b3c37df5d
Stanislav <33198664+StanislawVictorovich@users.noreply.github.com>
2019-06-27 09:57:11 +0300

    Update formatcase.js

--- FAT-2175/formatcase.js
+++ FAT-2175/formatcase.js
@@ -5,3 +5,12 @@
 function toCamelCase($s) {
   return $s.split('-').map(str => str = str[0].toUpperCase() + str.slice(1)).join('');
 }
+
+// the way of using reduce
+/* function toCamelCase($s) {
+  return $s.split('')
+    .reduce((result, item, i, arr) => 
+      item === '-' 
+        ? (arr[i+1] = arr[i+1].toUpperCase(), result) 
+        : result + item);
+} */
